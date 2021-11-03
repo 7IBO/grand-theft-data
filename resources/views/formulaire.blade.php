d97e7ada8fb7da0ce15664a9c27920b1a9c86951
Blackonde <93601223+Blackonde@users.noreply.github.com>
2021-11-03 16:02:28 +0100

    formulaire

---
+++ resources/views/formulaire.blade.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+</head>
+<body class='d-flex justify-content-center align-items-center'>
+    <form method='POST'>
+    <h4>Entré vôtre prénom : 
+    <input type='Text' id='Prenom' class='Prenom' placeholder='Entré vôtre prénom'/></h4>
+    <h4>Entré vôtre Nom : 
+    <input type='Text' id='Nom' class='Nom' placeholder='Entré vôtre Nom'/></h4>
+    <h4>Entré vôtre Mail : 
+    <input type='Text' id='Mail' class='Mail' placeholder='Entré vôtre Mail'/></h4>
+    <h4>Mots de passe : 
+    <input type='Text' id='MDP' class='MDP' placeholder='Mots de passe'/></h4>
+
+    <input type='submit' id='Valid' class='Valid' value='Validé'/></form>
+</body
