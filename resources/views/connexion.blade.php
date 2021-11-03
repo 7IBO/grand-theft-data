bab73dfe57dffebdb5610609d956a4b44ce010a0
Blackonde <93601223+Blackonde@users.noreply.github.com>
2021-11-03 16:41:32 +0100

    connexion

---
+++ resources/views/connexion.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body class="d-flex justify-content-center align-items-center" style="min-height: 100vh">
+    <form method="POST">
+        @csrf
+
+        <label class="form-label" for="email">Entrez votre Mail :</label>
+        <input type="email" id="email" name="email" class="form-control" placeholder="Entrez votre email"/>
+
+        <label class="form-label" for="password">Mot de passe :</label>
+        <input type="password" id="password" name="password" class="form-control" placeholder="Mot de passe"/>
+
+        <input type="submit" id="submit" class="btn btn-success mt-4" value="Connexion"/>
+    </form>
+</body>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('formulaire');
+    return view('connexion');
 });
 
 Route::post('/', [\App\Http\Controllers\UserController::class, 'store']);
