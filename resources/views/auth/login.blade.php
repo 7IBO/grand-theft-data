bf035b1e25028e2f7a40e698e0a5593a49281f11
Blackonde <93601223+Blackonde@users.noreply.github.com>
2021-11-04 10:48:06 +0100

    404

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-    <title>Document</title>
-</head>
-<body class="d-flex justify-content-center align-items-center" style="min-height: 100vh">
+@extends('layouts.app')
+
+@section('content')
+<div class="d-flex justify-content-center align-items-center" style="min-height: 100vh">
 
     <form method="POST">
         @csrf
@@ -20,4 +14,5 @@
 
         <input type="submit" id="submit" class="btn btn-success mt-4" value="Connexion"/>
     </form>
-</body>
+</div>
+@endsection
