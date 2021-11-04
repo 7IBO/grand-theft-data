79fdc5a9eb60ba72f8ea4c95d65894132ff9f8e6
Blackonde <93601223+Blackonde@users.noreply.github.com>
2021-11-04 14:23:35 +0100

    friends list

--- app/Http/Controllers/FriendshipController.php
+++ app/Http/Controllers/FriendshipController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Friendship;
 
 class FriendshipController extends Controller
 {
     public function index() {
-        $friends = \Auth::user()->friends;
+        $friends = Friendship::where('requester_id',\Auth::user()->id)->orWhere('addressee_id',\Auth::user()->id)->get();
         return view('friends', compact('friends'));
     }
 }
--- resources/views/friends.blade.php
+++ resources/views/friends.blade.php
@@ -2,10 +2,55 @@
 
 @section('content')
 
-    <div class="container mt-5">
-        @foreach ($friends as $friend)
-            {{$friend}}
-        @endforeach
-    </div>
+
+<div class="container mt-5">
+
+<table class="table table-danger mt-3">
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Pokemon</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            @foreach (\App\Models\Friendship::where('accepted_at', null)->where('addressee_id',\Auth::user()->id)->get() as $friend)
+            <tr>
+                <th>{{$friend->requester->first_name}} {{$friend->requester->last_name}}</th>
+                <th>{{$friend->requester->pokemon->name}}</th>
+                <th><a>Accepter</a> / <a>Refuser</a></th>
+            </tr>
+            @endforeach
+        </tbody>
+    
+    </table>
+
+
+    <table class="table table-secondary mt-3">
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Ami depuis ...</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach (\App\Models\Friendship::where('requester_id',\Auth::user()->id)->orWhere('addressee_id',\Auth::user()->id)->get() as $friend)
+            @if ($friend->requester_id==\Auth::user()->id)
+            <tr>
+                <th>{{$friend->addressee->first_name}} {{$friend->addressee->last_name}}</th>
+                <th>{{$friend->accepted_at}}</th>
+            </tr>
+
+            @else
+            <tr>
+                <th>{{$friend->requester->first_name}} {{$friend->requester->last_name}}</th>
+                <th>{{$friend->accepted_at}}</th>
+            </tr>
+            @endif
+            
+            @endforeach
+        </tbody>
+    
+    </table>
 
 @endsection
