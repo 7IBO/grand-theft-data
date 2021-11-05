3c5bdaa339158a176a691b146f1a8cbf30f1b626
7IBO <thibault.fayard.2001@gmail.com>
2021-11-05 08:37:17 +0100

    correction bug pokemons migration

--- database/migrations/2021_11_03_141138_create_pokemons_table.php
+++ database/migrations/2021_11_03_141138_create_pokemons_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePokemonTable extends Migration
+class CreatePokemonsTable extends Migration
 {
     /**
      * Run the migrations.
